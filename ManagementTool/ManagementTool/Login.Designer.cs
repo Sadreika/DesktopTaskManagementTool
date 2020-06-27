5e30c5e8128f3997733157aa034241001884dcdf
MrMariu5 <marius.sadreika@gmail.com>
2020-06-27 16:47:22 +0300

    Base

---
+++ ManagementTool/ManagementTool/Login.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace ManagementTool
+{
+    partial class Login
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Text = "Login";
+        }
+
+        #endregion
+    }
+}
---
+++ ManagementTool/ManagementTool/Main.Designer.cs
@@ -0,0 +1,26 @@
+﻿namespace ManagementTool
+{
+    partial class ManagementToolDesktop
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 600);
+            this.Text = "ManagementToolDesktop";
+        }
+        #endregion
+    }
+}
+
