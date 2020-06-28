8600d7cc2f23b542dd1dcd8dd35a15d07a41d250
MrMariu5 <marius.sadreika@gmail.com>
2020-06-28 19:20:21 +0300

    Bug fixed

--- ManagementTool/ManagementTool/RegistrationForm.cs
+++ ManagementTool/ManagementTool/RegistrationForm.cs
@@ -23,48 +23,76 @@ namespace ManagementTool
             SqlConnection con = new SqlConnection(connectionString);
             try
             {
+                bool usernameAndPasswordCorrectLenght = true;
                 con.Open();
                 System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
                 cmd.CommandType = System.Data.CommandType.Text;
                 cmd.Connection = con;
                 string id = searchingForMaxId();
                 string username = usernameTextbox.Text.ToString();
-                string password = passwordTextbox.Text.ToString();
-                string email = emailTextbox.Text.ToString();
-
-                if(seachingIfSameValueExists("username", username).Equals(false))
+                if(username.Length < 3)
                 {
-                    if(seachingIfSameValueExists("email", email).Equals(false))
-                    {
-                        try
-                        {
-                            cmd.Parameters.AddWithValue("@id", (Int16.Parse(id) + 1).ToString());
-                            cmd.Parameters.AddWithValue("@username", username);
-                            cmd.Parameters.AddWithValue("@password", password);
-                            cmd.Parameters.AddWithValue("@email", email);
-                            cmd.Parameters.AddWithValue("@isActive", "active");
+                    MessageBox.Show("Username is too short");
+                    usernameAndPasswordCorrectLenght = false;
+                }
+                string password = passwordTextbox.Text.ToString();
+                if (password.Length < 3)
+                {
+                    MessageBox.Show("Password is too short");
+                    usernameAndPasswordCorrectLenght = false;
+                }
+                string email = emailTextbox.Text.ToString();
+                if(email.Length < 5)
+                {
+                    MessageBox.Show("Email is too short");
+                    usernameAndPasswordCorrectLenght = false;
+                }
+                if(email.Contains('@'))
+                {
 
-                            cmd.CommandText = "INSERT [ManagementToolDatabase].[dbo].[UserRegistrationTable] (id, username, password, email, isActive)" +
-                                " VALUES (@id, @username, @password, @email, @isActive)";
-                            cmd.ExecuteNonQuery();
-                            MessageBox.Show("Registration completed");
-                            this.Hide();
-                            Login login = new Login();
-                            login.Show();
-                        }
-                        catch (Exception)
+                }
+                else
+                {
+                    MessageBox.Show("Wrong email type");
+                    usernameAndPasswordCorrectLenght = false;
+                }
+
+                if(usernameAndPasswordCorrectLenght.Equals(true))
+                {
+                    if (seachingIfSameValueExists("username", username).Equals(false))
+                    {
+                        if (seachingIfSameValueExists("email", email).Equals(false))
                         {
-                            MessageBox.Show("Registration is unavailable");
+                            try
+                            {
+                                cmd.Parameters.AddWithValue("@id", (Int16.Parse(id) + 1).ToString());
+                                cmd.Parameters.AddWithValue("@username", username);
+                                cmd.Parameters.AddWithValue("@password", password);
+                                cmd.Parameters.AddWithValue("@email", email);
+                                cmd.Parameters.AddWithValue("@isActive", "active");
+
+                                cmd.CommandText = "INSERT [ManagementToolDatabase].[dbo].[UserRegistrationTable] (id, username, password, email, isActive)" +
+                                    " VALUES (@id, @username, @password, @email, @isActive)";
+                                cmd.ExecuteNonQuery();
+                                MessageBox.Show("Registration completed");
+                                this.Hide();
+                                Login login = new Login();
+                                login.Show();
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Registration is unavailable");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Email is already taken");
                         }
                     }
                     else
                     {
-                        MessageBox.Show("Email is already taken");
+                        MessageBox.Show("Name is already taken");
                     }
-                }
-                else
-                {
-                    MessageBox.Show("Name is already taken");
                 }
                 con.Close();
             }
